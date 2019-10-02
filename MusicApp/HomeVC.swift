b39429e1d2cf33feee96741af419e32076b96a21
trung <thetrung2911@gmail.com>
2019-10-02 10:09:15 +0700

    Start

---
+++ MusicApp/HomeVC.swift
@@ -0,0 +1,22 @@
+//
+//  HomeVC.swift
+//  MusicApp
+//
+//  Created by Trung Le on 9/23/19.
+//  Copyright © 2019 Trung Le. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+    }
+    
+
+   
+
+}
---
+++ MusicApp/SearchVC.swift
@@ -0,0 +1,30 @@
+//
+//  SearchVC.swift
+//  MusicApp
+//
+//  Created by Trung Le on 9/23/19.
+//  Copyright © 2019 Trung Le. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
